ab341ffd68221aa6fe461eaf0806ce8f7d2af5d2
Williane Marques (DHL Supply Chain) <Williane.Marques@dhl.com>
2020-08-15 15:35:41 -0300

    Propriedades e tipos

--- JavaScript-OO/SistemaAutenticacao.js
+++ JavaScript-OO/SistemaAutenticacao.js
@@ -1,5 +1,16 @@
+import { Funcionario } from "./funcionarios/Funcionario";
+
 export class SistemaAutenticacao{
     static login(autenticavel, senha){
-        return autenticavel.autenticar(senha);
+        if(SistemaAutenticacao.ehAutenticavel(autenticavel)){
+
+            return autenticavel.autenticar(senha);
+        }
+
+        return false;
+    }
+
+    static ehAutenticavel(autenticavel){
+        return 'autenticar' in autenticavel && autenticavel.autenticar instanceof Function;
     }
 }
