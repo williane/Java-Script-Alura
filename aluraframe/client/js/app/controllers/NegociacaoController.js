6083be3195532a1d1fc140c8a241e74ada33c650
Williane Marques (DHL Supply Chain) <Williane.Marques@dhl.com>
2020-08-16 18:42:45 -0300

    Criando lista de Negociações e blindando o array

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -4,16 +4,30 @@ class NegociacaoController {
     this._inputData = $("#data");
     this._inputQuantidade = $("#quantidade");
     this._inputValor = $("#valor");
+    this._listaNegociacoes = new ListaNegociacoes();
   }
   adiciona(event) {
     event.preventDefault();
+    
+    this._listaNegociacoes.adiciona(this._criaNegociacao());
+    this._limpaFormulario();
 
-    let negociacao = new Negociacao(
+    console.log(this._listaNegociacoes.negociacoes);
+  }
+
+  _criaNegociacao() {
+    return new Negociacao(
       DateHelper.textoParadata(this._inputData.value),
       this._inputQuantidade.value,
       this._inputValor.value
     );
+  }
 
-    console.log(DateHelper.dataParaTexto(negociacao.data));
+  _limpaFormulario() {
+    this._inputData.value = "";
+    this._inputQuantidade.value = 1;
+    this._inputValor.value = 0.0;
+
+    this._inputData.focus();
   }
 }
