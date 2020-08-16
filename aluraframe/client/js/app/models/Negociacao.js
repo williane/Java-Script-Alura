6667e90fd59cac75b617903a88e697e96c82a557
Williane Marques (DHL Supply Chain) <Williane.Marques@dhl.com>
2020-08-16 12:29:19 -0300

    Programação defensiva

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -1,13 +1,13 @@
 class Negociacao {
     constructor(data, quantidade, valor){
-        this._data = data;
+        this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
         Object.freeze(this); //Congela o objeto depois de instanciado, não deixa alterar as propriedades
     }
 
     get data(){
-        return this._data;
+        return new Date(this._data.getTime());
     }
 
     get quantidade (){
