4ed07a353f12b2e37c5b7f4536a4b5b8ddf5fffd
Williane Marques (DHL Supply Chain) <Williane.Marques@dhl.com>
2020-08-16 18:10:50 -0300

    Template string e validação de data com regex

--- aluraframe/client/js/app/helpers/DateHelper.js
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -1,18 +1,22 @@
 class DateHelper {
   constructor() {
-    throw new Error('A classe DateHelper não deve ser instanciada');
+    throw new Error("A classe DateHelper não deve ser instanciada");
   }
 
   static textoParadata(texto) {
+    if (!/\d{4}[-/.]\d{1,2}[-/.]\d{1,2}/.test(texto)) {
+      throw new Error("o formato de data está inválido!");
+    }
+
     return new Date(
       //... spread desmembra o array
-      ...texto.split("-").map((item, indice) => (indice == 1 ? item - 1 : item))
+      ...texto
+        .split(/[-/.]/)
+        .map((item, indice) => (indice == 1 ? item - 1 : item))
     );
   }
 
   static dataParaTexto(data) {
-    return (
-      data.getDate() + "/" + (data.getMonth() + 1) + "/" + data.getFullYear()
-    );
+    return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
   }
 }
