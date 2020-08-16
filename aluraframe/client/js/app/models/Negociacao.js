8698e3b41a5794fd00bd0489207c6b811a404f73
Williane Marques (DHL Supply Chain) <Williane.Marques@dhl.com>
2020-08-16 11:44:14 -0300

    object freeze

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -1,7 +1,24 @@
 class Negociacao {
-    constructor(){
-        this.data = new Date();
-        this.quantidade = 1;
-        this.valor = 0;
+    constructor(data, quantidade, valor){
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this); //Congela o objeto depois de instanciado, não deixa alterar as propriedades
+    }
+
+    get data(){
+        return this._data;
+    }
+
+    get quantidade (){
+        return this._quantidade;
+    }
+
+    get valor (){
+        return this._valor;
+    }
+
+    get volume(){
+        return this.quantidade * this.valor;
     }
 }
