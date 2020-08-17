d8e0fa879adcc1675cad9905cf2c44589356d716
Williane Marques (DHL Supply Chain) <Williane.Marques@dhl.com>
2020-08-17 12:15:42 -0300

    Metodo abstrato

--- aluraframe/client/js/app/views/View.js
+++ aluraframe/client/js/app/views/View.js
@@ -3,7 +3,9 @@ class View{
         this._elemento = elemento;
       }
     
-      _template(model) {}
+      _template(model) {
+          throw new Error ('O método template deve ser sobescrito!');
+      }
     
       update(model) {
         this._elemento.innerHTML = this._template(model);
