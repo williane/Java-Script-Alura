42fc8eb4bc5c5d47c56c27da86f7f4afabdad665
Williane Marques (DHL Supply Chain) <Williane.Marques@dhl.com>
2020-08-17 11:08:04 -0300

    Usando Reduce

--- aluraframe/client/js/app/views/NegociacoesView.js
+++ aluraframe/client/js/app/views/NegociacoesView.js
@@ -31,15 +31,7 @@ class NegociacoesView {
 
             <tfoot>
                   <td colspan="3"></td>
-                  <td>                  
-                  ${
-                    /*IIFE - uma function que se auto invoca*/
-                    (function () {
-                      let total = 0;
-                      model.negociacoes.forEach((n) => (total += n.volume));
-                      return total;
-                    })()
-                  }</td> 
+                  <td>${model.negociacoes.reduce((total, n) => total + n.volume, 0.0)}</td> 
             </tfoot>
         </table>
     `;
