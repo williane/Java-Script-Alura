8ad2f186df2a097765b989d88d19ca9deed492e8
Williane Marques (DHL Supply Chain) <Williane.Marques@dhl.com>
2020-08-18 10:41:46 -0300

    Padrão Proxy na ListaNegociacoes

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -4,7 +4,20 @@ class NegociacaoController {
     this._inputData = $("#data");
     this._inputQuantidade = $("#quantidade");
     this._inputValor = $("#valor");
-    this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model));
+    let self = this;
+    this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+    
+      get(target, prop, receiver) {
+          if(['adiciona','esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function)){
+              return function(){
+                Reflect.apply(target[prop], target, arguments);
+                self._negociacoesView.update(target);
+              }
+          }
+          return Reflect.get(target, prop, receiver);
+      }
+  });
+
     this._negociacoesView = new NegociacoesView($('#negociacoesView'));
     this._mensagem = new Mensagem();
     this._mensagemView = new mensagemView($('#mensagemView'));
