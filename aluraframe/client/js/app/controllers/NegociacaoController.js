d3530b4b02275d63a477f9d1d2322cebeb0a4efb
Williane Marques (DHL Supply Chain) <Williane.Marques@dhl.com>
2020-08-18 13:11:01 -0300

    Padrão de Projeto Factory

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -4,43 +4,23 @@ class NegociacaoController {
     this._inputData = $("#data");
     this._inputQuantidade = $("#quantidade");
     this._inputValor = $("#valor");
-    let self = this;
-    this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
-    
-      get(target, prop, receiver) {
-          if(['adiciona','esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function)){
-              return function(){
-                Reflect.apply(target[prop], target, arguments);
-                self._negociacoesView.update(target);
-              }
-          }
-          return Reflect.get(target, prop, receiver);
-      }
-  });
-
+    this._listaNegociacoes = ProxyFactory.create(new ListaNegociacoes(),['adiciona', 'esvazia'], model => this._negociacoesView.update(model));
     this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-    this._mensagem = new Mensagem();
+    this._mensagem = ProxyFactory.create(new Mensagem(),['texto'], model => this._mensagemView.update(model));
     this._mensagemView = new mensagemView($('#mensagemView'));
 
     this._negociacoesView.update(this._listaNegociacoes);
   }
   adiciona(event) {
-    event.preventDefault();
-    
+    event.preventDefault();    
     this._listaNegociacoes.adiciona(this._criaNegociacao());
-
     this._mensagem.texto = 'Negociacao adicionada com sucesso!'
-    this._mensagemView.update(this._mensagem);
     this._limpaFormulario();
-
-    console.log(this._listaNegociacoes.negociacoes);
   }
 
   apaga(){
     this._listaNegociacoes.esvazia();
-
     this._mensagem.texto = 'Negociações apagadas com sucesso!';
-    this._mensagemView.update(this._mensagem);
   }
 
   _criaNegociacao() {
