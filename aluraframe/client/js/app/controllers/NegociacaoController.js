d94a7869eb642c87ecd73e9145b639afb51313ec
Williane Marques (DHL Supply Chain) <Williane.Marques@dhl.com>
2020-08-18 13:43:29 -0300

    Class Bind q retorna outra instancia no construtor

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -4,13 +4,12 @@ class NegociacaoController {
     this._inputData = $("#data");
     this._inputQuantidade = $("#quantidade");
     this._inputValor = $("#valor");
-    this._listaNegociacoes = ProxyFactory.create(new ListaNegociacoes(),['adiciona', 'esvazia'], model => this._negociacoesView.update(model));
     this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-    this._mensagem = ProxyFactory.create(new Mensagem(), ['texto'], model => this._mensagemView.update(model));
+    this._listaNegociacoes = new Bind(new ListaNegociacoes(), this._negociacoesView, ['adiciona', 'esvazia']);
     this._mensagemView = new mensagemView($('#mensagemView'));
-
-    this._negociacoesView.update(this._listaNegociacoes);
+    this._mensagem = new Bind(new Mensagem(), this._mensagemView, ['texto']);
   }
+  
   adiciona(event) {
     event.preventDefault();    
     this._listaNegociacoes.adiciona(this._criaNegociacao());
