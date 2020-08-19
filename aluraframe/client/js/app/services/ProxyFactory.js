60d9f7c5ca69f2580db518b858610259bbd3448b
Williane Marques (DHL Supply Chain) <Williane.Marques@dhl.com>
2020-08-19 12:06:30 -0300

    Melhorias na ProxyFactory deixando classe generica

--- aluraframe/client/js/app/services/ProxyFactory.js
+++ aluraframe/client/js/app/services/ProxyFactory.js
@@ -4,19 +4,18 @@ class ProxyFactory{
             get(target, prop, receiver) {
                 if(props.includes(prop) && ProxyFactory._ehFuncao(target[prop]) ){
                     return function(){
-                      Reflect.apply(target[prop], target, arguments);
-                      return acao(target);
+                      let retorno = Reflect.apply(target[prop], target, arguments);
+                      acao(target);
+                      return retorno;
                     }
                 }
                 return Reflect.get(target, prop, receiver);
             },
 
             set(target, prop, value, receiver){
-                if(props.includes(prop)){
-                    target[prop] = value;
-                    acao(target);
-                }
-                return Reflect.set(target, prop, value, receiver);
+                let retorno = Reflect.set(target, prop, value, receiver)
+                if(props.includes(prop)) acao(target);
+                return retorno;
             }
         });
     }
