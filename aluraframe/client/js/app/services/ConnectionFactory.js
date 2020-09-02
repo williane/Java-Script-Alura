1e5c8f32f54031f50a7d5d5286b12c8e43eabf74
Williane Marques (DHL Supply Chain) <Williane.Marques@dhl.com>
2020-09-02 11:27:38 -0300

    O padrão de projeto Module Pattern

--- aluraframe/client/js/app/services/ConnectionFactory.js
+++ aluraframe/client/js/app/services/ConnectionFactory.js
@@ -1,37 +1,44 @@
-var stores = ['negociacoes'];
-var version = 3;
-var dbName = 'aluraframe';
+var ConnectionFactory = (function (){
 
-class ConnectionFactory {
-
-    constructor() {
-        throw new Error('Não é possivel criar instancias de ConnectionFactory');
+    var stores = ['negociacoes'];
+    var version = 4;
+    var dbName = 'aluraframe';
+    var connection = null;
+    
+    return class ConnectionFactory {
+    
+        constructor() {
+            throw new Error('Não é possivel criar instancias de ConnectionFactory');
+        }
+    
+        static getConnection() {
+            return new Promise((resolve, reject) => {
+                let openRequest = window.indexedDB.open(dbName, version);
+    
+                openRequest.onupgradeneeded = e => {
+                    ConnectionFactory._createStores(e.target.result);
+                };
+    
+                openRequest.onsuccess = e => {
+                    if(!connection) connection = e.target.result;
+                    resolve(connection);
+                };
+    
+                openRequest.onerror = e => {
+                    console.log(e.target.error);
+                    reject(e.target.error.name);
+                };
+            });
+        }
+    
+        static _createStores(connection) {
+            stores.forEach(store => {
+                connection.objectStoreNames.contains(store) && connection.deleteObjectStore(store);
+                connection.createObjectStore(store, { autoIncrement: true });
+            });
+        }
+    
     }
+})();
 
-    static getConnection() {
-        return new Promise((resolve, reject) => {
-            let openRequest = window.indexedDB.open(dbName, version);
 
-            openRequest.onupgradeneeded = e => {
-                ConnectionFactory._createStores(e.target.result);
-            };
-
-            openRequest.onsuccess = e => {
-                resolve(e.target.result);
-            };
-
-            openRequest.onerror = e => {
-                console.log(e.target.error);
-                reject(e.target.error.name);
-            };
-        });
-    }
-
-    static _createStores(connection) {
-        stores.forEach(store => {
-            connection.objectStoreNames.contains(store) && connection.deleteObjectStore(store);
-            connection.createObjectStore(store, { autoIncrement: true });
-        });
-    }
-
-}
