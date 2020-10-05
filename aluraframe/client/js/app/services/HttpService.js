e0cd5759a36fdb2d3c72ca29b4d51ebdf5526a67
Williane Marques (DHL Supply Chain) <Williane.Marques@dhl.com>
2020-10-04 22:12:49 -0300

    Utilizando a Fetch API

--- aluraframe/client/js/app/services/HttpService.js
+++ aluraframe/client/js/app/services/HttpService.js
@@ -1,37 +1,29 @@
 class HttpService {
 
-    get(url) {
-        return new Promise((resolve, reject) => {
+    _handleErrors(res) {
+        if (!res.ok) throw new Error(res.statusText);
+        return res;
 
-            let xhr = new XMLHttpRequest();
-
-            xhr.open('GET', url);
-
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState == 4) {
-                    if (xhr.status == 200) {
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-
-            xhr.send();
-        });
     }
 
-    post(url, dado){
+    get(url) {
+
+        return fetch(url)
+            .then(res => this._handleErrors(res))
+            .then(res => res.json());
+    }
+
+    post(url, dado) {
 
         return new Promise((resolve, reject) => {
             let xhr = new XMLHttpRequest();
             xhr.open('POST', url, true);
             xhr.setRequestHeader('Content-Type', 'application/json');
             xhr.onreadystatechange = () => {
-                if(xhr.readyState == 4){
-                    if(xhr.status == 200){
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
                         resolve(JSON.parse(xhr.responseText));
-                    }else{
+                    } else {
                         reject(xhr.responseText);
                     }
                 }
